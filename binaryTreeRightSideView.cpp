accf250e151896aaa278ae9ad3138b8814656ff5
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-06-01 01:48:56 +0530

    init

---
+++ binaryTreeRightSideView.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        int c=0;
+        q1.push(root);
+        vector<vector<int>> out;
+        if(root==NULL)
+            return out;
+        
+        while(!q1.empty() or !q2.empty())
+        {
+            vector<int> temp;
+            if(c%2==0)
+            {
+                while(!q1.empty())
+                {
+                    TreeNode* curr = q1.front();
+                    q1.pop();
+                    temp.push_back(curr->val);
+                    if(curr->left!=NULL)
+                    {
+                        q2.push(curr->left);
+                    }
+                    if(curr->right!=NULL)
+                    {
+                        q2.push(curr->right);
+                    }
+                    
+                }
+                out.push_back(temp);
+                temp.clear();
+            }
+            else
+            {
+                while(!q2.empty())
+                {
+                    TreeNode* curr = q2.front();
+                    q2.pop();
+                    temp.push_back(curr->val);
+                    if(curr->left!=NULL)
+                    {
+                        q1.push(curr->left);
+                    }
+                    if(curr->right!=NULL)
+                    {
+                        q1.push(curr->right);
+                    }
+                }
+                out.push_back(temp);
+                temp.clear();
+            }
+            c++;
+        }
+        return out;
+    }
+    
+    vector<int> rightSideView(TreeNode* root) 
+    {
+        vector<vector<int>> arr = levelOrder(root);
+        vector<int> out;
+        for(auto x: arr)
+        {
+            out.push_back(x[x.size()-1]);
+        }
+        return out;
+        
+    }
+};
