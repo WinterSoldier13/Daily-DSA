8966165f77dc6865220e045d92d2bc8f8ddef522
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-06-09 20:17:20 +0530

    init

---
+++ invertBinaryTree.cpp
@@ -0,0 +1,37 @@
+
+//function Template for C++
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+/* Should convert tree to its mirror */
+void mirror(Node* node) 
+{
+     // Your Code Here
+     
+     if(node == NULL)
+     return;
+     
+     Node* left = node->left;
+     Node* right = node->right;
+     Node* temp = left;
+     
+     node->left = right;
+     node->right = temp;
+     mirror(node->left);
+     mirror(node->right);
+     
+     
+     
+}
