bc875ac30924ff55c2b1dc861c096d8424a20186
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-06-10 23:59:05 +0530

    init

---
+++ heightBalancedTree.cpp
@@ -0,0 +1,44 @@
+// A binary tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+// This function should return tree if passed  tree 
+// is balanced, else false. 
+int height(Node* root)
+{
+    if(root == 0)
+    {
+        return 0;
+    }
+
+    return 1+max(height(root->left), height(root->right));
+}
+
+
+bool isBalanced(Node *root)
+{
+    if(root == NULL)
+    {
+        return true;
+    }
+    
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    
+    if((abs(leftHeight-rightHeight)<=1) and isBalanced(root->left) and isBalanced(root->right))
+    {
+        return true;
+    }
+    return false;
+}
