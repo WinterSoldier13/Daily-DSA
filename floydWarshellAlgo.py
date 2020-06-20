108fa496e4aa9f509476814f3921ad499238aaae
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-06-20 13:06:13 +0530

    init

---
+++ floydWarshellAlgo.py
@@ -0,0 +1,30 @@
+#code
+t=int(input())
+while(t!=0):
+	n=int(input())
+
+	temp=[0]*n
+	graph = [temp]*n
+
+	for i in range(n):
+		line = input()
+		line = line.split(' ')
+		temp=[]
+		for x in line:
+			temp.append(int(x))
+		graph[i] = temp
+
+# 	print(graph)
+	dist = graph
+
+	for k in range(0,n):
+		for i in range(0,n):
+			for j in range(0,n):
+				if(dist[i][j]> dist[i][k] + dist[k][j]):
+					dist[i][j] = dist[i][k] + dist[k][j]
+
+	for i in range(n):
+		for j in range(n):
+			print(str(dist[i][j]),end=" ")
+		print()
+	t-=1
