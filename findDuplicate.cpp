c430987b11e6f8e0f7a3f24d2e5762736a7e2924
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-07-02 01:13:53 +0530

    init

--- findDuplicate.cpp
+++ findDuplicate.cpp
@@ -14,3 +14,24 @@ public:
         
     }
 };
+
+
+
+// Another way
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+        
+    }
+};
