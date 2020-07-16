734dffddb902ee37c8e7ebc218c9decae1602238
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-07-17 01:21:25 +0530

    init

---
+++ removeLinkedListElements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *curr= head,*prev = head;
+        while(curr){
+            if(curr->val ==val && prev->val ==val){
+                prev=prev->next;
+                head = head->next;
+                curr->next=nullptr;
+                delete curr;
+                curr = prev;
+                continue;
+            }
+            else if(curr->val==val){
+                prev->next = curr->next;
+                curr->next = nullptr;
+                delete curr;
+                curr = prev;
+            }
+            
+            prev = curr;
+            curr=curr->next;
+        }
+        return head;
+
+    }
+};
