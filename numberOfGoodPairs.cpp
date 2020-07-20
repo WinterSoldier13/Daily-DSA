8267237dd399161ff30cd683ca262f98804ce719
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-07-20 11:46:09 +0530

    init

---
+++ numberOfGoodPairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) 
+    {
+        set<pair<int,int>> hashset;
+        
+        int l = nums.size();
+        if(l==0)
+            return 0;
+        
+        for(int i=0;i<l;i++)
+        {
+            for(int j=i+1;j<l;j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    hashset.insert(make_pair(i,j));
+                }
+            }
+        }
+        return hashset.size();
+        
+        
+    }
+};
