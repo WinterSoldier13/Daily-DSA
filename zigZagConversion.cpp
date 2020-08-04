1f7fb68eb2432bb4fdc57aaf8134284fe4f1b946
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-04 22:35:20 +0530

    init

---
+++ zigZagConversion.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+string convert(string s, int numRows)
+{
+    if(numRows == 1)
+    {
+        return s;
+    }
+    if(numRows == 0)
+    {
+        return "";
+    }
+    vector<vector<char>> arr(numRows);
+
+    int start = 0;
+    int end = numRows - 1;
+    int c = 0;
+    int index = 0;
+
+    for (auto ch : s)
+    {
+        if (index > end)
+        {
+            index = end - 1;
+            c++;
+        }
+        else if (index < start)
+        {
+            index = start + 1;
+            c++;
+        }
+
+        if (c % 2 == 0)
+        {
+            arr[index++].push_back(ch);
+        }
+        else
+        {
+            arr[index--].push_back(ch);
+        }
+    }
+    string __word__ = "";
+    for (auto x : arr)
+    {
+        for (auto c : x)
+        {
+            cout << c << " ";
+            __word__ += c;
+        }
+        cout << endl;
+    }
+    return __word__;
+}
+};
