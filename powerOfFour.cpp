fe03de4869bf49bf7d2c92cb93d752f658fbdbce
WinterSoldier <ayushsingh1315@gmail.com>
2020-08-04 15:19:18 +0530

    init

---
+++ powerOfFour.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+bool isPowerOfFour(int num)
+{
+    if(num == 0)
+    {
+        return false;
+    }
+    int index = 0;
+    bool digitFound = false;
+    while (num != 0)
+    {
+        if (num & 1)
+        {
+            if (index % 2 != 0)
+            {
+                return false;
+            }
+            if(!digitFound)
+            {
+                digitFound = true;
+            }
+            else if(digitFound)
+            {
+                return false;
+            }
+            
+        }
+        num = num >> 1;
+        index++;
+    }
+    return true;
+}
+};
