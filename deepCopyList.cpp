4d66de9d3fa13e47d50701bf2f86c7286ecd32eb
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-07 23:16:50 +0530

    init

---
+++ deepCopyList.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        unordered_map<Node*, Node*> hashmap;
+
+        Node* temp = head;
+
+        while(temp!=NULL)
+        {
+            Node* node = new Node(temp->val);
+            hashmap[temp] = node;
+            temp = temp->next;
+        }
+        cout<<hashmap.size()<<endl;
+        temp = head;
+        while (temp!=NULL)
+        {
+            hashmap[temp]->random = hashmap[temp->random];
+            temp = temp->next;
+        }
+        temp = head;
+        while(temp!=NULL)
+        {
+            hashmap[temp]->next = hashmap[temp->next];
+            temp = temp->next;
+        }
+        return hashmap[head];
+        
+    }
+};
