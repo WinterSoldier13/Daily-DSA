7f122040cb526ccc24370471fd5eef711c5d0a33
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-10 13:20:42 +0530

    init

--- codeChefAugLong5.cpp
+++ codeChefAugLong5.cpp
@@ -1,125 +1,89 @@
 #include <bits/stdc++.h>
-#include <stdio.h>
+#include<stdio.h>
 using namespace std;
-#define lli long long int
-
-int recurse(vector<int> &arr, int index, int currentCost, unordered_set<int> hashset, int k)
-{
-
-    if (index == 0)
-    {
-        return currentCost;
-    }
-
-    int curr = arr[index - 1];
-    unordered_set<int> hashset2 = hashset;
-    unordered_set<int> hashset3;
-    int found = 0;
-
-    if (hashset2.count(curr))
-    {
-        found = 1;
-    }
-
-    hashset2.insert(curr);
-    int cost1, cost2;
-    if (found)
-    {
-        cost1 = recurse(arr, index - 1, currentCost + 1, hashset2, k);
-    }
-    else
-    {
-        cost1 = recurse(arr, index - 1, currentCost, hashset2, k);
-    }
-
-    cost2 = recurse(arr, index - 1, currentCost + k, hashset3, k);
-
-    return min(cost1, cost2);
-}
-
-int dynamic(vector<int> &arr, int k)
-{
-    int n = arr.size();
-    vector<int> dp(n, 0);
-    dp[0] = k;
-
-    int bareMin = k;
-    unordered_map<int, int> hashmapTemp;
-    
-
-    unordered_map<int, int> hashmap;
-    hashmap[arr[0]]++;
-
-
-
-    for (int i = 1; i < n; i++)
-    {
-        int occur = 0;
-        if (hashmap.find(arr[i]) != hashmap.end())
-        {
-            occur = hashmap[arr[i]];
-            if (occur == 1)
-            {
-                // dp[i] = min(dp[i - 1] + k, dp[i - 1] + 2);
-                if(dp[i-1]+k  < dp[i-1]+2)
-                {
-                    dp[i] = dp[i-1]+k;
-                    hashmap.clear();
-                }
-                else
-                {
-                    dp[i] = dp[i-1] +2;
-                }
-                
-            }
-            else
-            {
-                // dp[i] = min(dp[i - 1] + k, dp[i - 1] + 1);
-                 if(dp[i-1]+k  < dp[i-1]+2)
-                {
-                    dp[i] = dp[i-1]+k;
-                    hashmap.clear();
-                }
-                else
-                {
-                    dp[i] = dp[i-1] +1;
-                }
-            }
-        }
-        else
-        {
-            dp[i] = dp[i - 1];
-        }
-
-        hashmap[arr[i]]++;
-    }
-
-    return dp[n - 1];
-}
+#define ll long long int
 
 int main()
 {
-    // int t;
-    // scanf("%d", &t);
 
-    // while (t--)
-    // {
-    //     int n, k;
-    //     scanf("%d %d", &n, &k);
-    //     vector<int> arr(n);
+    int t;
+    scanf("%d", &t);
 
-    //     for (int i = 0; i < n; i++)
-    //     {
-    //         scanf("%d", &arr[i]);
-    //     }
-    //     unordered_set<int> hashmap;
-    //     cout <<"RECURSION " <<1 + recurse(arr, arr.size(), k, hashmap, k) << endl;
-    //     cout << "DYNAMIC " <<dynamic(arr, k) << endl;
-    // }
+    while (t--)
+    {
+        ll n, k;
+        scanf("%lld %lld",&n, &k);
+        ll a[n + 1];
+        map<ll, ll> mp;
+        ll ans = 0, vc = 0, t = 1;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
 
-    unordered_set<int> hashmap;
-    vector<int> arr = {1,2,1,2,1,2,1,2,1,2,1};
-    int k = 1;
+        ll col[n + 1][n + 1] = {0};
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                col[i][j] = 0;
 
-    cout << 1+recurse(arr, arr.size(), k, hashmap, k) << endl;
+        for (int i = 0; i < n; i++)
+        {
+            mp.clear();
+
+            for (int j = i; j < n; j++)
+            {
+                col[i][j] = (j == 0) ? 0 : col[i][j - 1];
+                if (mp.count(a[j]))
+                {
+                    if (mp[a[j]] == 1)
+                    {
+                        col[i][j]++;
+                    }
+                    col[i][j]++;
+                }
+                mp[a[j]]++;
+            }
+        }
+
+        ans = 1e18;
+        ll table = 100;
+        ll dp[101][1001] = {0};
+        for (int i = 0; i <= table; i++)
+        {
+            for (int j = 0; j <= n; j++)
+                dp[i][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            dp[1][i] = col[0][i - 1];
+        }
+
+        for (int i = 1; i <= table; i++)
+        {
+            dp[i][1] = 0;
+        }
+
+        for (int i = 2; i <= table; i++)
+        {
+            for (int j = 2; j <= n; j++)
+            {
+                ll bestval = 1e18;
+
+                for (int p = 1; p < j; p++)
+                {
+                    bestval = min(bestval, dp[i - 1][p] + col[p][j - 1]);
+                }
+                dp[i][j] = bestval;
+            }
+        }
+
+        for (table = 1; table <= 100; table++)
+        {
+            ans = min(table * k + dp[table][n], ans);
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
 }
