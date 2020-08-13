72974b43fb1587bc629bbbc185081013758ad536
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-13 12:39:14 +0530

    init

---
+++ longestValidParanthesis.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+int longestValidParentheses(string s)
+{
+    stack<int> stack;
+    stack.push(-1);
+
+    int l = s.size();
+    int maxLen = 0;
+
+    for(int i=0;i<l;i++)
+    {
+        char ch = s[i];
+
+        if(ch == '(')
+        {
+            stack.push(i);
+        }
+        else
+        {
+            int stackTop = stack.top();
+            if(stackTop!=-1){
+            char atTop = s[stackTop];
+
+            if(atTop == '(')
+            {
+                stack.pop();
+                maxLen = max(maxLen, i-stack.top());
+            }
+            else
+            {
+                stack.push(i);
+            }}
+            else
+                stack.push(i);
+        }
+    }
+    cout<<maxLen<<endl;
+    return maxLen;
+}
+};
