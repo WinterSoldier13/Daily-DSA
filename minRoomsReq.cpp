100ffb4adc7b310a66be8119c39f8f4db12d4cec
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-15 16:41:30 +0530

    improved readability

--- minRoomsReq.cpp
+++ minRoomsReq.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int t;
-    cin>>t;
+    cin >> t;
 
-    while(t--)
+    while (t--)
     {
         // int n;
         // cin>>n;
@@ -18,12 +18,12 @@ int main()
         // for(int i=0 ;i<n;i++)
         // cin>>end_[i];
 
-        vector<int> start{1,3,0,5,8,5};
-        vector<int>  end_{2,4,6,7,9,9};
+        vector<int> start{1, 3, 0, 5, 8, 5};
+        vector<int> end_{2, 4, 6, 7, 9, 9};
         int n = 6;
 
         vector<pair<int, int>> slots;
-        for (int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
             slots.push_back(make_pair(start[i], end_[i]));
         }
@@ -34,50 +34,48 @@ int main()
 
         vector<vector<pair<int, int>>> rooms;
 
-        for(auto slot: slots)
+        for (auto slot : slots)
         {
             int currentBegin = slot.first;
             int currentEnd = slot.second;
-            cout<<"CURRENT: "<<currentBegin<<" "<<currentEnd<<endl;
-            if(rooms.empty())
+            cout << "CURRENT: " << currentBegin << " " << currentEnd << endl;
+            if (rooms.empty())
             {
-                vector<pair<int,int>> temp;
+                vector<pair<int, int>> temp;
                 temp.push_back(make_pair(currentBegin, currentEnd));
-                rooms.push_back(temp); 
-                continue; 
+                rooms.push_back(temp);
+                continue;
             }
             bool found = false;
-            
-            for(int i=0;i<rooms.size();i++)
+
+            for (int i = 0; i < rooms.size(); i++)
             {
                 auto room = rooms[i];
-                int prevBegin = room[room.size()-1].first;
-                int prevEnd   = room[room.size()-1].second;
+                int prevBegin = room[room.size() - 1].first;
+                int prevEnd = room[room.size() - 1].second;
 
-                if(currentBegin>prevEnd)
+                if (currentBegin > prevEnd)
                 {
-                    rooms[i].push_back(make_pair(currentBegin,currentEnd));
+                    rooms[i].push_back(make_pair(currentBegin, currentEnd));
                     found = true;
                     break;
                 }
             }
-            if(!found)
+            if (!found)
             {
-                vector<pair<int,int>> temp;
+                vector<pair<int, int>> temp;
                 temp.push_back(make_pair(currentBegin, currentEnd));
                 rooms.push_back(temp);
             }
         }
 
-
-        for(auto x: rooms)
+        for (auto x : rooms)
         {
-            for(auto c:x)
+            for (auto c : x)
             {
-                cout<<c.first<<" "<<c.second<<" || ";
+                cout << c.first << " " << c.second << " || ";
             }
-            cout<<endl;
+            cout << endl;
         }
-
     }
 }
