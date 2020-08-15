1223b9d09d843e0ffb41f7f55df05de22191316d
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-15 12:44:51 +0530

    init

--- longestIncreasingSubSeq.cpp
+++ longestIncreasingSubSeq.cpp
@@ -1,57 +1,54 @@
-class Solution {
+class Solution
+{
 public:
-    
     int recurse(vector<int> &arr, int index)
     {
-        if(index==0)
+        if (index == 0)
         {
             return 0;
         }
-        if(arr[index-1]>arr[index-2])
+        if (arr[index - 1] > arr[index - 2])
         {
-            return 1+ recurse(arr, index-1);
+            return 1 + recurse(arr, index - 1);
         }
-        else{
-            return recurse(arr,index-1);
+        else
+        {
+            return recurse(arr, index - 1);
         }
     }
-    
+
     int dp(vector<int> arr)
     {
         int l = arr.size();
-        vector<int> dp(l,1);
+        vector<int> dp(l, 1);
 
         dp[0] = 1;
 
-        for(int i=1;i<l;i++)
+        for (int i = 1; i < l; i++)
         {
             int curr = arr[i];
             int maxL = 0;
-            for(int j =i-1;j>=0;j--)
+            for (int j = i - 1; j >= 0; j--)
             {
-                if(arr[j]<curr)
+                if (arr[j] < curr)
                 {
-                    maxL = max(maxL, dp[j]);
+                    dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
-            dp[i] = ++maxL;
-
         }
         int maxEle = -1;
-        for(auto x: dp)
-	        maxEle = max(maxEle, x);
+        for (auto x : dp)
+            maxEle = max(maxEle, x);
 
-	    return maxEle;
+        return maxEle;
     }
 
-    
-    int lengthOfLIS(vector<int>& arr) 
+    int lengthOfLIS(vector<int> &arr)
     {
-        if((int)arr.size()==0)
+        if ((int)arr.size() == 0)
         {
             return 0;
         }
         return dp(arr);
-        
     }
 };
