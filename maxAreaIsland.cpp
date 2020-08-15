69ee4a94494c3ab321747fbbff2ffb0c1075adea
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-15 15:07:40 +0530

    init

--- maxAreaIsland.cpp
+++ maxAreaIsland.cpp
@@ -1,40 +1,40 @@
-class Solution {
+class Solution
+{
 public:
     int recurse(vector<vector<int>> &arr, int i, int j)
-{
-    int rows = arr.size();
-    int cols = arr[0].size();
-
-    if(i<0 or j<0 or i>=rows or j>=cols or arr[i][j]!=1)
     {
-        return 0;
-    }
-    arr[i][j] = -1;
-    return 1+recurse(arr,i-1,j)+recurse(arr,i,j+1)+recurse(arr,i+1,j)+recurse(arr,i,j-1);
-}
+        int rows = arr.size();
+        int cols = arr[0].size();
 
-int counter(vector<vector<int>> &arr)
-{
-    int area =0;
-    int rows = arr.size();
-    int cols = arr[0].size();
-
-    for(int i = 0;i<rows;i++)
-    {
-        for(int j=0;j<cols;j++)
+        if (i < 0 or j < 0 or i >= rows or j >= cols or arr[i][j] != 1)
         {
-            if(arr[i][j]==1)
+            return 0;
+        }
+        arr[i][j] = -1;
+        return 1 + recurse(arr, i - 1, j) + recurse(arr, i, j + 1) + recurse(arr, i + 1, j) + recurse(arr, i, j - 1);
+    }
+
+    int counter(vector<vector<int>> &arr)
+    {
+        int area = 0;
+        int rows = arr.size();
+        int cols = arr[0].size();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
             {
-                area = max(area, recurse(arr, i,j));
+                if (arr[i][j] == 1)
+                {
+                    area = max(area, recurse(arr, i, j));
+                }
             }
         }
+        return area;
     }
-    return area;
-}
 
-    int maxAreaOfIsland(vector<vector<int>>& grid) 
+    int maxAreaOfIsland(vector<vector<int>> &grid)
     {
         return counter(grid);
-        
     }
 };
