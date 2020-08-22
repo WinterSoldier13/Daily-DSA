d5ed9ba3021174d4c1253e8e76ef3724b8c10013
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-22 13:21:05 +0530

    init

---
+++ randomPickWithWeight.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+#define lli long long int
+
+class Solution
+{
+public:
+    vector<int> cummulativeFreq;
+
+    Solution(vector<int> &w)
+    {
+        vector<int> temp(w.size());
+        cummulativeFreq = temp;
+
+        cummulativeFreq[0] = w[0];
+        for (int i = 1; i < w.size(); i++)
+        {
+            cummulativeFreq[i] = w[i] + cummulativeFreq[i - 1];
+        }
+    }
+
+    int pickIndex()
+    {
+        int num = 1 + rand() % cummulativeFreq[cummulativeFreq.size() - 1];
+
+        int index = 0;
+
+        for (auto x : cummulativeFreq)
+        {
+            if (num < x)
+            {
+                return index;
+            }
+            index++;
+        }
+        return 0;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
