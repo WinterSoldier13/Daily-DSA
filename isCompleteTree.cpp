4cbe81e1dc466fb8afa6140ebae40e3444f6e163
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-23 19:06:36 +0530

    init

---
+++ isCompleteTree.cpp
@@ -0,0 +1,34 @@
+
+class Solution
+{
+public:
+    bool isCompleteTree(TreeNode *root)
+    {
+        queue<TreeNode *> q;
+        q.push(root);
+        int nullFound = 0;
+        while (!q.empty())
+        {
+            auto curr = q.front();
+            q.pop();
+
+            if (curr == NULL)
+            {
+                nullFound = 1;
+            }
+            else
+            {
+                if (nullFound)
+                {
+                    return 0;
+                }
+                else
+                {
+                    q.push(curr->left);
+                    q.push(curr->right);
+                }
+            }
+        }
+        return 1;
+    }
+};
