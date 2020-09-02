b24832a05206e9f67d82c8f8943eabdf25efde15
WinterSoldier <ayushsingh1315@gmail.com>
2020-09-02 19:38:21 +0530

    init

--- swapNodesInPair.cpp
+++ swapNodesInPair.cpp
@@ -1,38 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+//   Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution
 {
 public:
     ListNode *swapPairs(ListNode *head)
     {
         int l = 0;
-        ListNode* temp = head;
-        while(temp!=NULL)
+        ListNode *temp = head;
+        while (temp != NULL)
         {
             temp = temp->next;
             l++;
         }
-        if(l == 0 or l == 1)
+        if (l == 0 or l == 1)
         {
             return head;
         }
-        ListNode* dummyNode = new ListNode();
+        ListNode *dummyNode = new ListNode();
         dummyNode->next = head;
-        ListNode* prev = dummyNode;
-        ListNode* curr = head;
+        ListNode *prev = dummyNode;
+        ListNode *curr = head;
 
-        while(curr!=NULL and curr->next!=NULL)
+        while (curr != NULL and curr->next != NULL)
         {
-            ListNode* mark1 = curr->next;
-            ListNode* mark2 = curr->next->next;
+            ListNode *mark1 = curr->next;
+            ListNode *mark2 = curr->next->next;
 
             curr->next->next = curr;
             curr->next = mark2;
@@ -43,6 +44,5 @@ public:
         }
 
         return dummyNode->next;
-
     }
 };
