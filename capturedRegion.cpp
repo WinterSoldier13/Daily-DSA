d630d11bc48532e662369b861e941db1482225dd
WinterSoldier <ayushsingh1315@gmail.com>
2020-09-09 13:04:13 +0530

    init

--- capturedRegion.cpp
+++ capturedRegion.cpp
@@ -1,143 +1,128 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
 // https://www.interviewbit.com/problems/capture-regions-on-board/
 
-void replaceO(vector<vector<string>>& board)
+void replaceO(vector<vector<string>> &board)
+{
+    int rows = board.size();
+    int cols = board[0].size();
+
+    for (int i = 0; i < rows; i++)
     {
-        int rows = board.size();
-        int cols = board[0].size();
-        
-        for(int i=0;i<rows;i++)
+        for (int j = 0; j < cols; j++)
         {
-            for(int j=0;j<cols;j++)
-            {
-                if(board[i][j]=="O")
+            if (board[i][j] == "O")
                 board[i][j] = "A";
-            }
         }
-        
     }
-    
-    void recurse(vector<vector<string>>& grid, int i, int j)
+}
+
+void recurse(vector<vector<string>> &grid, int i, int j)
+{
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    if (i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] != "A")
     {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        
-        if(i<0 or j<0 or i>=rows or j>=cols or grid[i][j]!="A")
-        {
-            return;
-        }
-        grid[i][j] = "O";
-        
-        recurse(grid, i-1,j);
-        recurse(grid, i,j+1);
-        recurse(grid, i+1,j);
-        recurse(grid, i,j-1);
-        
+        return;
     }
-    
-    void replaceEdge(vector<vector<string>> &board)
+    grid[i][j] = "O";
+
+    recurse(grid, i - 1, j);
+    recurse(grid, i, j + 1);
+    recurse(grid, i + 1, j);
+    recurse(grid, i, j - 1);
+}
+
+void replaceEdge(vector<vector<string>> &board)
+{
+    int rows = board.size();
+    int cols = board[0].size();
+
+    for (int i = 0; i < cols; i++)
     {
-        int rows = board.size();
-        int cols = board[0].size();
-        
-        for(int i=0;i<cols;i++)
+        if (board[0][i] == "A")
         {
-            if(board[0][i] == "A")
-            {
-                recurse(board, 0,i);
-            }
+            recurse(board, 0, i);
         }
-        for(int i=0;i<cols;i++)
-        {
-            if(board[rows-1][i] == "A")
-            {
-                recurse(board, rows-1,i);
-            }
-        }
-        
-        for(int i=0;i<rows;i++)
-        {
-            if(board[i][0] == "A")
-            {
-                recurse(board, i, 0);
-            }
-        }
-        for(int i=0;i<rows;i++)
-        {
-            if(board[i][cols-1] == "A")
-            {
-                recurse(board, i,cols-1);
-            }
-        }
-        
-        
     }
-    
-    
-    void replaceA(vector<vector<string>>& board)
+    for (int i = 0; i < cols; i++)
     {
-        int rows = board.size();
-        int cols = board[0].size();
-        
-        for(int i =0;i<rows;i++)
+        if (board[rows - 1][i] == "A")
         {
-            for(int j=0;j<cols;j++)
-            {
-                if(board[i][j] == "A")
-                    board[i][j] = "X";
-            }
+            recurse(board, rows - 1, i);
         }
-        
     }
 
-
-    void print(vector<vector<string>>& grid)
+    for (int i = 0; i < rows; i++)
     {
-        for(auto x: grid)
+        if (board[i][0] == "A")
         {
-            for(auto c: x)
-            cout<<c<<" ";
-            cout<<endl;
-            
+            recurse(board, i, 0);
         }
-        cout<<endl;
-
     }
-    
-    void solve(vector<vector<string>>& board) 
+    for (int i = 0; i < rows; i++)
     {
-        
-        replaceO(board);
-        print(board);
-        replaceEdge(board);
-        print(board);
-        replaceA(board);
-        print(board);
-
-        
+        if (board[i][cols - 1] == "A")
+        {
+            recurse(board, i, cols - 1);
+        }
     }
+}
 
+void replaceA(vector<vector<string>> &board)
+{
+    int rows = board.size();
+    int cols = board[0].size();
 
-    int main()
+    for (int i = 0; i < rows; i++)
     {
-        vector<vector<char>> arr{{'X','X','X','X'},
-                                 {'X','O','O','X'},
-                                 {'X','X','O','X'},
-                                 {'X','O','X','X'}};
-
-        vector<vector<char>> arr2{{'X','O','X'},
-                                  {'O','X','O'},
-                                  {'X','O','X'}};
-
-
-        vector<vector<string>> arr3
-{{"X","O","O","X","X","X","O","X","O","O"},{"X","O","X","X","X","X","X","X","X","X"},{"X","X","X","X","O","X","X","X","X","X"},{"X","O","X","X","X","O","X","X","X","O"},{"O","X","X","X","O","X","O","X","O","X"},{"X","X","O","X","X","O","O","X","X","X"},{"O","X","X","O","O","X","O","X","X","O"},{"O","X","X","X","X","X","O","X","X","X"},{"X","O","O","X","X","O","X","X","O","O"},{"X","X","X","O","O","X","O","X","X","O"}};
-        print(arr3);
-
-        solve(arr3);
+        for (int j = 0; j < cols; j++)
+        {
+            if (board[i][j] == "A")
+                board[i][j] = "X";
+        }
     }
+}
+
+void print(vector<vector<string>> &grid)
+{
+    for (auto x : grid)
+    {
+        for (auto c : x)
+            cout << c << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void solve(vector<vector<string>> &board)
+{
+
+    replaceO(board);
+    print(board);
+    replaceEdge(board);
+    print(board);
+    replaceA(board);
+    print(board);
+}
+
+int main()
+{
+    vector<vector<char>> arr{{'X', 'X', 'X', 'X'},
+                             {'X', 'O', 'O', 'X'},
+                             {'X', 'X', 'O', 'X'},
+                             {'X', 'O', 'X', 'X'}};
+
+    vector<vector<char>> arr2{{'X', 'O', 'X'},
+                              {'O', 'X', 'O'},
+                              {'X', 'O', 'X'}};
+
+    vector<vector<string>> arr3{{"X", "O", "O", "X", "X", "X", "O", "X", "O", "O"}, {"X", "O", "X", "X", "X", "X", "X", "X", "X", "X"}, {"X", "X", "X", "X", "O", "X", "X", "X", "X", "X"}, {"X", "O", "X", "X", "X", "O", "X", "X", "X", "O"}, {"O", "X", "X", "X", "O", "X", "O", "X", "O", "X"}, {"X", "X", "O", "X", "X", "O", "O", "X", "X", "X"}, {"O", "X", "X", "O", "O", "X", "O", "X", "X", "O"}, {"O", "X", "X", "X", "X", "X", "O", "X", "X", "X"}, {"X", "O", "O", "X", "X", "O", "X", "X", "O", "O"}, {"X", "X", "X", "O", "O", "X", "O", "X", "X", "O"}};
+    print(arr3);
+
+    solve(arr3);
+}
