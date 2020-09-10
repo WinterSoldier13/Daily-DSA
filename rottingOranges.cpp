db88c5895410994555a4f91eb702d2490706774d
WinterSoldier <ayushsingh1315@gmail.com>
2020-09-10 21:17:00 +0530

    now It works

--- rottingOranges.cpp
+++ rottingOranges.cpp
@@ -1,109 +1,162 @@
-#include<iostream>
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
+#include <stdio.h>
 using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false)
+#define lli unsigned long long int
 
-struct cell
+// Rotten oranges
+
+class Node
 {
-    public:
+public:
     int val;
-    int timestamp;
-
-    cell(int v, int t)
+    int time;
+    int x;
+    int y;
+    Node()
     {
-        val = v;
-        timestamp = t;
+        time = 0;
+    }
+    Node(int a, int c, int z, int w)
+    {
+        val = a;
+        time = c;
+        x = z;
+        y = w;
     }
 };
 
+vector<vector<Node *>> buildBoard(vector<vector<int>> &arr)
+{
+    int rows = arr.size();
+    int cols = arr[0].size();
+
+    vector<vector<Node *>> board(rows, vector<Node *>(cols));
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Node *node = new Node(arr[i][j], INT16_MAX, i, j);
+            board[i][j] = node;
+        }
+    }
+    return board;
+}
 
 int bfs(vector<vector<int>> &arr)
 {
     int rows = arr.size();
     int cols = arr[0].size();
+    int time = 0;
 
-    vector<pair<int,int>> dirs{make_pair(-1,0),make_pair(0,1),make_pair(1,0),make_pair(0,-1)};
-  
-    vector<vector<cell>> grid;
-    for(int i=0;i<rows;i++)
+    vector<vector<Node *>> board = buildBoard(arr);
+
+    queue<Node *> q;
+    vector<vector<int>> track(rows, vector<int>(cols, 0));
+    // Pushing all the rotten oranges
+    for (int i = 0; i < rows; i++)
     {
-        vector<cell> temp;
-
-        for(int j =0 ;j<cols; j++)
+        for (int j = 0; j < cols; j++)
         {
-            cell x = cell(arr[i][j], 0);
-            temp.push_back(x);
-        }
-        grid.push_back(temp);
-    }
+            auto curr = board[i][j];
 
-    queue<pair<int,int>> q;
-
-    for(int i=0;i<rows;i++)
-    {
-        for(int j=0;j<cols;j++)
-        {
-            if(grid[i][j].val == 2)
+            if (curr->val == 2)
             {
-                q.push(make_pair(i,j));
+                curr->time = 0;
+                q.push(curr);
             }
         }
     }
 
-    while(!q.empty())
+    while (!q.empty())
     {
-        int currentX = q.front().first;
-        int currentY = q.front().second;
-        int currTimestamp = grid[currentX][currentY].timestamp;
-
+        auto currNode = q.front();
         q.pop();
 
-        for(auto x: dirs)
+        int currX = currNode->x;
+        int currY = currNode->y;
+        int currTime = currNode->time;
+
+        vector<int> dx{-1, 0, 1, 0};
+        vector<int> dy{0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++)
         {
-            int moveX = currentX + x.first;
-            int moveY = currentY + x.second;
+            int newX = currX + dx[i];
+            int newY = currY + dy[i];
 
-            if(moveX<0 or moveX>=rows or moveY<0 or moveY>=cols)
-            continue;
-
-            if(grid[moveX][moveY].val==1 and grid[moveX][moveY].timestamp == 0)
+            if (newX < 0 or newX >= rows or newY < 0 or newY >= cols or track[newX][newY] == 1 or board[newX][newY]->val != 1)
             {
-                if(grid[moveX][moveY].timestamp == 0)
-                {
-                    grid[moveX][moveY].timestamp++;
-                }
-                grid[moveX][moveY].val = 2;
-                grid[moveX][moveY].timestamp = max(grid[moveX][moveY].timestamp, currTimestamp+1);
+                continue;
+            }
+            else
+            {
+                auto oldNode = board[newX][newY];
+                oldNode->time = min(oldNode->time, currTime + 1);
+                oldNode->val = 2;
 
-                q.push(make_pair(moveX, moveY));
+                board[newX][newY] = oldNode;
+                q.push(oldNode);
+                track[newX][newY] = 1;
             }
         }
     }
-
-    //Search if any orrange is left unrotten
-    
-    int max_ = INT32_MIN;
-    for(int i = 0;i<rows;i++)
+    // Check if any oranges are left fresh
+    // if not then calculate the max time
+    time = 0;
+    for (int i = 0; i < rows; i++)
     {
-        for(int j=0;j<cols;j++)
+        for (int j = 0; j < cols; j++)
         {
-            max_ = max(max_ , grid[i][j].timestamp);
-            if(grid[i][j].val == 1)
+            // cout << " [" << board[i][j]->val << "," << board[i][j]->time << "] ";
+            if (board[i][j]->val == 1)
             {
                 return -1;
             }
+            if (board[i][j]->val == 2)
+                time = max(time, board[i][j]->time);
         }
+        // cout << endl;
     }
-    return max_;
+
+    return time;
 }
 
 int main()
 {
-    vector<vector<int>> arr{{2,1,1},
-                            {1,1,0},
-                            {0,1,1}};
+    fast_cin();
+    cin.tie(NULL);
 
-    cout<<bfs(arr)<<endl;
-    
-    return 0;
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int r, c;
+        cin >> r >> c;
+        if (r == 0 or c == 0)
+        {
+            cout << -1;
+            continue;
+        }
+
+        vector<vector<int>> arr(r, vector<int>(c, 0));
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+        // for (auto x : arr)
+        // {
+        //     for (auto c : x)
+        //         cout << c << " ";
+        //     cout << endl;
+        // }
+        cout << bfs(arr) << endl;
+    }
+    return 1;
 }
