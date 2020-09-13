c70f74d3fe8bb66ae7618e812bdb97945b2b2512
WinterSoldier <ayushsingh1315@gmail.com>
2020-09-13 15:27:05 +0530

    readingInput

---
+++ readingAnInputStringWithSpaces.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false)
+#define lli unsigned long long int
+#define pii pair<lli, int>
+
+int main()
+{
+    fast_cin();
+    cin.tie(0);
+    cout.tie(0);
+
+
+    // This line is important 
+    cin.ignore();
+    string s;
+    getline(cin, s);
+}
