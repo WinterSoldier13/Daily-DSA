00984dadaf4b46572299c2e5c6bd6cb9e694a71f
WinterSoldier <ayushsingh1315@gmail.com>
2020-10-07 19:35:35 +0530

    Shift+Alt+F

--- RotatedArraySearch.cpp
+++ RotatedArraySearch.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -8,136 +8,126 @@ int pivotElement(vector<int> &arr)
     int l = arr.size();
     int start = 0;
     int end = l;
-    int ans =0;
+    int ans = 0;
 
     //check for decreasing order
-    if(arr[0]>arr[1] and arr[l-2]>arr[l-1])
+    if (arr[0] > arr[1] and arr[l - 2] > arr[l - 1])
     {
-        ans =-1;
+        ans = -1;
     }
     else
     {
-        if(arr[0]>arr[l-1])
+        if (arr[0] > arr[l - 1])
         {
 
-            while(start<=end)
+            while (start <= end)
             {
-                int mid = start+ (end-start)/2;
-                
-                if(arr[mid]>arr[mid+1])
+                int mid = start + (end - start) / 2;
+
+                if (arr[mid] > arr[mid + 1])
                 {
-                    ans= mid+1;
+                    ans = mid + 1;
                     break;
                 }
-                if(arr[start] <= arr[mid])
+                if (arr[start] <= arr[mid])
                 {
-                    start = mid+1;
+                    start = mid + 1;
                 }
                 else
                 {
-                    end = mid-1;
+                    end = mid - 1;
                 }
-                
             }
         }
-        
     }
-    
+
     return ans;
 }
 
-
-int search(vector<int>& arr, int target)
+int search(vector<int> &arr, int target)
 {
-    if(arr.size() == 0)
+    if (arr.size() == 0)
     {
         return -1;
     }
-    if(arr.size() == 1 and target == arr[0])
+    if (arr.size() == 1 and target == arr[0])
     {
         return 0;
     }
-    else if(arr.size() == 1 )
+    else if (arr.size() == 1)
     {
         return -1;
     }
     int pivot = pivotElement(arr);
     int start = 0;
-    int end = arr.size()-1;
+    int end = arr.size() - 1;
 
-    if(!(pivot == 0))
+    if (!(pivot == 0))
     {
-        if(arr[pivot] == target)
+        if (arr[pivot] == target)
         {
             return pivot;
         }
-        if(target>arr[0])
+        if (target > arr[0])
         {
-            end = pivot -1;
+            end = pivot - 1;
         }
         else
         {
-            start = pivot +1;
+            start = pivot + 1;
         }
-        
     }
-    
-    if(pivot == -1)
+
+    if (pivot == -1)
     {
-        start = arr.size()-1;
+        start = arr.size() - 1;
         end = 0;
 
-        while(end<=start)
+        while (end <= start)
         {
-            int mid = end + (start - end)/2;
+            int mid = end + (start - end) / 2;
 
-            if(arr[mid] == target)
+            if (arr[mid] == target)
             {
                 return mid;
             }
-            else if(arr[mid]<target)
+            else if (arr[mid] < target)
             {
-                start = mid -1;
+                start = mid - 1;
             }
             else
             {
-                end= mid +1;
+                end = mid + 1;
             }
-            
-            
         }
         return -1;
-
     }
 
-    while(start<=end)
+    while (start <= end)
     {
-        int mid = start + (end-start)/2;
+        int mid = start + (end - start) / 2;
 
-        if(arr[mid] == target)
+        if (arr[mid] == target)
         {
             return mid;
         }
-        if(arr[mid]<target)
+        if (arr[mid] < target)
         {
-            start = mid +1;
+            start = mid + 1;
         }
         else
         {
-            end = mid-1;
+            end = mid - 1;
         }
-        
     }
     return -1;
-
 }
 
 int main()
 {
-    vector<int> arr{3,1};
+    vector<int> arr{3, 1};
 
-    cout<<search(arr, 0)<<endl;
-
+    cout << search(arr, 0) << endl;
 
     return 0;
 }
