6fd78b016ee1c32ce4027965331e20e15641d332
WinterSoldier <ayushsingh1315@gmail.com>
2021-01-04 01:34:32 +0530

    init

--- relativeSorting.cpp
+++ relativeSorting.cpp
@@ -1,54 +1,71 @@
-class Solution
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false)
+#define lli unsigned long long int
+
+void solve(vector<int> arr1, vector<int> arr2)
 {
-public:
-    vector<int> relativeSortArray(vector<int> &arr1, vector<int> &arr2)
+    int l1 = arr1.size();
+    int l2 = arr2.size();
+
+    unordered_map<int, int> hashmap;
+    for (auto x : arr1)
+    {
+        hashmap[x]++;
+    }
+
+    vector<int> out;
+
+    for (auto x : arr2)
+    {
+        for (int i = 0; i < hashmap[x]; i++)
+        {
+            out.push_back(x);
+        }
+        hashmap.erase(hashmap.find(x));
+    }
+
+    vector<int> temp;
+    for (auto it = hashmap.begin(); it != hashmap.end(); it++)
+    {
+        for (int i = 0; i < it->second; i++)
+        {
+            temp.push_back(it->first);
+        }
+    }
+    sort(temp.begin(), temp.end());
+
+    for (auto x : temp)
+        out.push_back(x);
+
+    for (auto x : out)
+        cout << x << " ";
+    cout << endl;
+}
+
+int main()
+{
+    fast_cin();
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
     {
         int n, m;
-        n = arr1.size();
-        m = arr2.size();
+        cin>>n>>m;
 
-        unordered_set<int> track;
+        vector<int> arr1(n);
+        vector<int> arr2(m);
 
-        vector<int> out(n, 0);
+        for(int i=0;i<n;i++)
+        cin>>arr1[i];
+        for(int i=0;i<m;i++)
+        cin>>arr2[i];
 
-        unordered_map<int, int> hashmap;
-
-        for (auto x : arr1)
-        {
-            hashmap[x]++;
-        }
-        int index = 0;
-
-        for (int i = 0; i < arr2.size(); i++)
-        {
-            int curr = arr2[i];
-            bool flag = false;
-
-            while (hashmap[curr]--)
-            {
-                flag = true;
-                out[index++] = curr;
-            }
-            if (flag)
-                track.insert(curr);
-        }
-
-        vector<int> leftover;
-        for (auto x : arr1)
-        {
-            if (track.find(x) == track.end())
-                leftover.push_back(x);
-        }
-        sort(leftover.begin(), leftover.end());
-
-        for (auto x : leftover)
-        {
-            out[index++] = x;
-        }
-        for (auto x : out)
-            cout << x << " ";
-
-        cout << endl;
-        return out;
+        solve(arr1, arr2);
     }
-};
+    return 1;
+}
