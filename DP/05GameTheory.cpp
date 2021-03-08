de1835181f9f954337e4ed60875eb0505c3391eb
WinterSoldier13 <ayushsingh1315@gmail.com>
2021-03-08 15:55:16 +0530

    init

---
+++ DP/05GameTheory.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define getT() \
+    int t;     \
+    cin >> t;  \
+    while (t--)
+#define lli unsigned long long int
+#define pii pair<int, int>
+
+// https://practice.geeksforgeeks.org/problems/pots-of-gold-game/1/?category[]=Dynamic%20Programming&category[]=Dynamic%20Programming&problemStatus=unsolved&difficulty[]=1&page=1&sortBy=submissions&query=category[]Dynamic%20ProgrammingproblemStatusunsolveddifficulty[]1page1sortBysubmissionscategory[]Dynamic%20Programming
+
+class Solution
+{
+    public:
+    vector<vector<int>> recursionCache;
+
+    Solution()
+    {
+        recursionCache = vector<vector<int>> (1001, vector<int> (1001, -1));
+    }
+    int recurse(int arr[], int start, int end)
+    {
+        if(recursionCache[start][end] != -1)
+        {
+            return recursionCache[start][end]=recursionCache[start][end];
+        }
+        if (end - start == 1)
+        {
+            return recursionCache[start][end]=max(arr[start], arr[end]);
+        }
+        if (end - start == 0)
+        {
+            return recursionCache[start][end]=arr[start];
+        }
+
+        return recursionCache[start][end]=max(arr[start] + min(recurse(arr, start + 1, end - 1), recurse(arr, start + 2, end)), arr[end] + min(recurse(arr, start + 1, end - 1), recurse(arr, start, end - 2)));
+    }
+};
+int maxCoins(int arr[], int N)
+{
+    Solution ob;
+    return ob.recurse(arr, 0, N - 1);
+}
+int main()
+{
+    fast_cin();
+
+    return 0;
+}
