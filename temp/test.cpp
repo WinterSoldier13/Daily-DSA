0ca94d9291efde14ea7d58d62d3b9abb0d0c8a9b
WinterSoldier13 <ayushsingh1315@gmail.com>
2021-07-14 19:27:22 +0530

    init

--- temp/test.cpp
+++ temp/test.cpp
@@ -1,75 +1,74 @@
 #include <bits/stdc++.h>
-#include <stdio.h>
-#include <stdlib.h>
 using namespace std;
 #define fast_cin()                    \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL)
-#define getT() \
-    int t;     \
-    cin >> t;  \
-    while (t--)
-#define lli long long int
-#define pii pair<int, int>
-#define endl '\n'
-#define printArr(arr)     \
-    for (auto x : arr)    \
-        cout << x << " "; \
-    cout << endl;
-#define printHashmap(hashmap)                                  \
-    for (auto it = hashmap.begin(); it != hashmap.end(); it++) \
-    {                                                          \
-        cout << it->first << " " << it->second << endl;        \
+
+class Solution {
+public:
+    int trap(vector<int>& arr) 
+    {
+        int l = arr.size();
+        if(l == 0 or l==1) return 0;
+        vector<int> left;
+        int ans = 0;
+        
+        left.push_back(arr[0]);
+        for(int i=1;i<l;i++)
+            left.push_back(max(left.back(), arr[i]));
+        int right = arr.back();
+        for(int i=l-2;i>=0;i--)
+        {
+            right = (max(right, arr[i]));
+            ans+= (min(left[i], right) - arr[i]);
+        }
+        return ans;
     }
-#define print2DArray(arr)     \
-    for (auto x : arr)        \
-    {                         \
-        for (auto c : x)      \
-            cout << c << " "; \
-        cout << endl;         \
+    int solve(string s)
+    {
+    	vector<int> arr;
+    	int l = s.size();
+
+    	int ptr = 0;
+    	string temp = "";
+    	while(ptr<l)
+    	{
+    		if(isdigit(s[ptr]))
+    		{
+    			while(isdigit(s[ptr]))
+    			{
+    				temp.push_back(s[ptr]);
+    				ptr++;
+    			}
+    		}
+    		else
+    		{
+    			if(temp!="")
+    			{
+    				arr.push_back(stoi(temp));
+    				temp = "";
+    			}
+    			ptr++;
+    		}
+    	}
+        if(temp!="")
+        {
+            arr.push_back(stoi(temp));
+            temp = "";
+        }
+        for(auto x: arr)
+            cout<<x<<" ";
+        cout<<endl;
+    	return trap(arr);
     }
-
-// _______________________________________________________________________________________________________________________________________________________________________________
-// _______________________________________________________________________________________________________________________________________________________________________________
-// _______________________________________________________________________________________________________________________________________________________________________________
-
-
-#include <bits/stdc++.h>
-using namespace std;
-#define fast_cin()                    \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL)
-#define ll long long
-#define endl "\n"
-unordered_map<ll, ll> parent;
-
-ll find(ll s)
-{
-    if(parent[s] == s)
-        return s;
-    return parent[s] = find(parent[s]);
-}
+};
 
 int main()
 {
 	fast_cin();
 
-    ll n;
-    cin>>n;
-    for(ll i = 0; i < n;i++){
-        ll x;
-        cin>>x;
-        parent[x] = x;
-    }
-    ll edge;
-    cin>>edge;
-    for(ll i = 0; i < edge;i++){
-        ll x,y;
-        cin>>x>>y;
-        parent[find(x)] = find(y);
-    }
-    ll a,b;
-    cin>>a>>b;
-    cout<< (find(a) == find(b))<<endl;
+	string s = "4 2 0 3 2 52";
+    Solution ob;
+    cout<<ob.solve(s)<<endl;
 	return 0;
 }
--- temp/test2.cpp
+++ temp/test2.cpp
@@ -6,91 +6,9 @@ using namespace std;
 #define ll int
 #define endl "\n"
 
-void dijstra(vector<vector<ll>> &graph, ll start, ll end, unordered_map<ll, ll> &pairNode)
-{
-	ll n = graph.size();
-	vector<ll> dist(n, INT_MAX);
-	unordered_set<ll> visited;
-	priority_queue<pair<ll, ll>> pq;
-	unordered_map<ll, ll> parentMap;
-	dist[start] = 0;
-	pq.push({0, start});
-
-	while (!pq.empty())
-	{
-		auto curr = pq.top();
-		pq.pop();
-
-		ll currNode = curr.second;
-
-		if (visited.count(currNode))
-			continue;
-		visited.insert(currNode);
-
-		for (ll i = 0; i < n; i++)
-		{
-			if (graph[currNode][i] == -1)
-				continue;
-
-			if (dist[i] > dist[currNode] + graph[currNode][i])
-			{
-				dist[i] = dist[currNode] + graph[currNode][i];
-				pq.push({-dist[i], i});
-				parentMap[i] = currNode;
-			}
-		}
-	}
-	int current = end;
-	while (current != start)
-	{
-		if (parentMap[current] == start)
-		{
-			cout << pairNode[current] << endl;
-			return;
-		}
-		current = parentMap[current];
-	}
-	cout<<-1<<endl;
-}
-
 int main()
 {
 	fast_cin();
-	ll n;
-	cin >> n;
-	ll currentNode = 0;
-	unordered_map<ll, ll> pairNode;
-	unordered_map<ll, ll> inversePairNode;
-
-	for (ll i = 0; i < n; i++)
-	{
-		ll id;
-		cin >> id;
-		pairNode[id] = currentNode;
-		inversePairNode[currentNode] = id;
-		currentNode++;
-	}
-
-	ll edges;
-	cin >> edges;
-	vector<vector<ll>> graph(currentNode, vector<ll>(currentNode, -1));
-
-	for (ll i = 0; i < edges; i++)
-	{
-		ll u, v, t;
-		cin >> u >> v >> t;
-		u = pairNode[u];
-		// v = pairNode[v];
-		graph[u][v] = t;
-		graph[v][u] = t;
-	}
-
-	ll a, b;
-	cin >> a >> b;
-	a = pairNode[a];
-	b = pairNode[b];
-
-	dijstra(graph, a, b, inversePairNode);
-
+	
 	return 0;
 }
